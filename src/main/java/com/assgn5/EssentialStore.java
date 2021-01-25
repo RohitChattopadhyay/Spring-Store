98fd40388ee9c02f7c6c93aa7e3b349520808d42
RohitChattopadhyay <rohit.chattopadhyay1@gmail.com>
2021-01-26 02:05:18 +0530

    Spring Boot Initialize

---
+++ src/main/java/com/assgn5/EssentialStore.java
@@ -0,0 +1,12 @@
+package com.assgn5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EssentialStore {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EssentialStore.class, args);
+	}
+}
