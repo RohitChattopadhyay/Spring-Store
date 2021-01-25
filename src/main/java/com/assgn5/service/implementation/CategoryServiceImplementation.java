b13555004bd6bb12b0bf083115b74f8eaa5784ce
RohitChattopadhyay <rohit.chattopadhyay1@gmail.com>
2021-01-26 02:07:03 +0530

    Create Implementation

---
+++ src/main/java/com/assgn5/service/implementation/CategoryServiceImplementation.java
@@ -0,0 +1,21 @@
+package com.assgn5.service.implementation;
+
+import com.assgn5.dao.CategoryRepository;
+import com.assgn5.dto.CategoryResponse;
+import com.assgn5.model.Category;
+import com.assgn5.service.CategoryService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service    
+public class CategoryServiceImplementation implements CategoryService {
+    @Autowired
+    private CategoryRepository categoryRepository;
+    
+    @Override
+    public CategoryResponse getById(long categoryId){
+        Category category = categoryRepository.getOne(categoryId);        
+        return CategoryResponse.from(category);
+    }
+}
---
+++ src/main/java/com/assgn5/service/implementation/OrderServiceImplementation.java
@@ -0,0 +1,28 @@
+package com.assgn5.service.implementation;
+
+import com.assgn5.dao.OrderRepository;
+import com.assgn5.dto.OrderRequest;
+import com.assgn5.dto.OrderResponse;
+import com.assgn5.model.Order;
+import com.assgn5.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderServiceImplementation implements OrderService {
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Override
+    public OrderResponse getById(long orderidentifier){
+        Order order = orderRepository.getOne(orderidentifier);        
+        return OrderResponse.from(order);
+    }
+
+    @Override
+    public OrderResponse create(OrderRequest orderRequest){
+        Order order = orderRequest.from();
+        order =  orderRepository.save(order);
+        return OrderResponse.from(order);
+    }
+}
